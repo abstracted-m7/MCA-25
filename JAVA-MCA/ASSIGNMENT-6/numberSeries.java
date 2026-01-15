3688d528fd2e2df0419abd26e4bf9217af8ca244
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2026-01-15 12:13:46 +0530

    Add files via upload

--- JAVA-MCA/ASSIGNMENT-6/numberSeries.java
+++ JAVA-MCA/ASSIGNMENT-6/numberSeries.java
@@ -28,6 +28,24 @@ class SeriesGenerator {
         System.out.println("Average of the array: "+avg);
     }
 
+    //find max & min from series
+    void findMaxMin(int [] arr){
+        
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i]; 
+            }
+            if (arr[i] > max) {
+                max = arr[i]; 
+            }
+        }
+        System.out.println("Minimum elemen is: " + min);
+        System.out.println("Maximum element is: " + max);
+    }
+
 }
 
 public class numberSeries {
@@ -43,6 +61,7 @@ public class numberSeries {
 
     int [] arr = obj.generateNumber(num);
     obj.calSumAvg(arr);
+    obj.findMaxMin(arr);
     System.out.println(Arrays.toString(arr));
   }  
 }
