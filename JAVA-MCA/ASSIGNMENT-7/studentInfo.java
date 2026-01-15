67007273d0f6178c324710f125009c25e2501a64
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2026-01-15 17:37:33 +0530

    Add files via upload

---
+++ JAVA-MCA/ASSIGNMENT-7/studentInfo.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+class studentData {
+    //name, age, course, and institution    
+    private String name, course, institution;
+    private int age;
+
+    studentData(String name, String course, String institution, int age){
+        this.name = name;
+        this.course = course;
+        this.institution = institution;
+        this.age = age;
+    }
+
+    //ShowData
+    void ShowData(){
+        System.out.println("==============================");
+        System.out.println("        STUDENT DETAILS       ");
+        System.out.println("==============================");
+        System.out.println("Name        :"+name);
+        System.out.println("Age:        :"+age);
+        System.out.println("Course      :"+course);
+        System.out.println("Institution :"+institution);
+        System.out.println("==============================");
+
+    }
+}
+
+
+public class studentInfo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String name, course, institution;
+        int age;
+
+        System.out.print("Enter Your Info: ");
+        System.out.print("Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Age: ");
+        age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Course: ");
+        course = sc.nextLine();
+
+        System.out.println("Institution: ");
+        institution = sc.nextLine();
+
+        studentData stud = new studentData(name, course, institution, age);
+
+        stud.ShowData();
+    }
+}
