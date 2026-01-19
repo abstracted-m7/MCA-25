f672df1495fab198db7bb8838bf187a19db7bc75
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2026-01-19 23:43:34 +0530

    Add files via upload

---
+++ JAVA-MCA/ASSIGNMENT-12/SumOfNNatural.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+class sumCalculator{
+
+    int claculate(int limit){
+        int sum = 0;
+        for (int i = 1; i <= limit; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
+
+public class SumOfNNatural {
+    public static void main(String[] args) {
+        int limit;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("N natural number sum calculator");
+
+        System.out.print("Enter the number (limit): ");
+        limit = sc.nextInt();
+        sc.close();
+
+        sumCalculator cal = new sumCalculator();
+        int result = cal.claculate(limit);
+        System.out.printf("Sum of %d natural number is : %d.",limit, result );
+        sc.close();
+    }
+}
