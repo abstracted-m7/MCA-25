f52ec8d5f50aa090f08a8aa84aa20009a7cef533
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2026-02-06 10:48:11 +0530

    Add files via upload

---
+++ DSA-MCA/ASSIGNMENT-2/QueueMenuDriven.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#define MAX 50
+
+int queue[MAX];
+int front = -1, rear = -1;
+void insert(){
+	int x;
+	if(rear == MAX-1) printf("Queue Overflow..!!");
+	else{
+		printf("\nEnter elements: ");
+		scanf("%d",&x);
+		if(front == -1) front = 0;
+		queue[++rear] = x;
+	}
+}
+
+void deleteElement(){
+	if(front == -1 || front > rear) printf("\nQueue Underflow..!!");
+	else printf("\nDeleted Element: %d\n",queue[front++]);
+}
+
+void display(){
+	int i;
+	if(front == -1 || front > rear) printf("\nQueue Empty..!!");
+	else
+		for(i = front; i <= rear; i++) printf("%d",queue[i]);
+		printf("\n");
+
+}
+
+int main(){
+	int ch;
+	do{
+		printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+		scanf("%d",&ch);
+		switch(ch){
+			case 1: insert();break;
+			case 2: deleteElement();break;
+			case 3: display();break;
+			case 4: printf("Thank you..!!");break;
+		}
+	}while(ch != 4);
+	return 0;
+}
