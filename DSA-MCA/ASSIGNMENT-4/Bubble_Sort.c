35e81879ee4035a10c3eff749b82ae4b3f836869
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2026-03-27 10:22:53 +0530

    Add files via upload

---
+++ DSA-MCA/ASSIGNMENT-4/Bubble_Sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    // Outer loop for the number of passes
+    for (i = 0; i < n - 1; i++) {
+        // Inner loop for comparisons in each pass
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap if the element found is greater than the next element
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Sorted list in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
